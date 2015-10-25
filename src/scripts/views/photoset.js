23ac563670618c409bbe34819bf031e20ea97bc0
AJ Livingston <livingston.aj@gmail.com>
2015-10-24 21:49:31 -0400

    lazy loading images

--- src/scripts/views/photoset.js
+++ src/scripts/views/photoset.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var _ = require('lodash');
 var Backbone = require('backbone');
 var Vent = require('../vent');
 var PhotosetTemplate = require('../templates/photoset.ejs');
@@ -20,16 +19,7 @@ module.exports = Backbone.View.extend({
     },
 
     render: function () {
-        var cover = this.model.get('cover');
-        var img = new Image();
-
         this.$el.html(this.template(this.model.toJSON()));
-
-        img.onload = _.bind(function () {
-            this.$('.photoset-contain').css('opacity', 1);
-        }, this);
-        img.src = cover.large.src;
-
         return this;
     },
 
--- src/scripts/views/portfolio.js
+++ src/scripts/views/portfolio.js
@@ -1,14 +1,20 @@
 'use strict';
 
+var $ = require('jquery');
 var Backbone = require('backbone');
 var PhotosetView = require('./photoset');
 var SlideshowView = require('./slideshow');
+
+require('lazyloadxt');
+$.lazyLoadXT.autoInit = false;
+$.lazyLoadXT.onload.addClass = 'animated fadeIn';
 
 module.exports = Backbone.View.extend({
 
     initialize: function () {
         this.$el.append(new SlideshowView({ collection: this.collection }).el);
         this.listenToOnce(this.collection, 'reset', this.render);
+
         return this;
     },
 
@@ -18,6 +24,8 @@ module.exports = Backbone.View.extend({
             var photosetView = new PhotosetView({ model: photoset });
             $photosets.append(photosetView.render().el);
         }, this);
+
+        this.$('.photoset-image').lazyLoadXT();
 
         return this;
     }
